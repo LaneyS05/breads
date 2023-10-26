fb16493a774c640674b5f3a2ba5406f6cd75da27
LaneyS05 <laney.staggs05@gmail.com>
2023-10-25 21:06:53 -0400

    create new routes complete

--- server.js
+++ server.js
@@ -6,31 +6,30 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
-})
-
- // Breads
- const breadsController = require('./controllers/breads_controller.js')
-const e = require('express')
- app.use('/breads', breadsController)
-
-// LISTEN
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-})
-
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
 
 // 404 Page
 app.get('*', (req, res) => {
   res.send('404')
 })
 
-
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
   
