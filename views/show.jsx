14012fe9f5ebc9c5a8fe00faa078d4dc7b3306ac
LaneyS05 <laney.staggs05@gmail.com>
2023-10-28 10:22:09 -0400

    delete route complete

--- views/show.jsx
+++ views/show.jsx
@@ -1,23 +1,24 @@
-const React = require('react')
-const Default = require('./layouts/Default')
+const React = require("react");
+const Default = require("./layouts/Default");
 
-function Show ({bread}) {
-    // Confirm we are getting our bread data in the terminal.
-    // console.log(bread.name)
-      return (
-        <Default>
-          <h2>Show Page</h2>
-          <h3>{bread.name}</h3>
-          <p>and it {
-            bread.hasGluten
-            ? <span> does </span>
-            : <span> does not </span>
-            }
-            have gluten 
-            </p>
-          <img src={bread.image} alt={bread.name} />
-        </Default>
-      )
-  }  
+function Show({ bread, index }) {
+  // Confirm we are getting our bread data in the terminal.
+  // console.log(bread.name)
+  return (
+    <Default>
+      <h2>Show Page</h2>
+      <h3>{bread.name}</h3>
+      <p>
+        and it {bread.hasGluten ? <span> does </span> : <span> does not </span>}
+        have gluten
+      </p>
+      <img src={bread.image} alt={bread.name} />
 
-module.exports = Show
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type="submit" value="DELETE" />
+      </form>
+    </Default>
+  );
+}
+
+module.exports = Show;
