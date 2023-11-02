45e861d486ce0b48f163fbdb2bf9aa910dc6ae74
LaneyS05 <laney.staggs05@gmail.com>
2023-11-01 20:58:54 -0400

    adding data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,72 +1,108 @@
-const express = require('express')
-const breads = express.Router()
-const Bread = require('../models/bread.js')
+const express = require("express");
+const breads = express.Router();
+const Bread = require("../models/bread.js");
 
 // NEW
-breads.get('/new', (req, res) => {
-  res.render('new')
-})
+breads.get("/new", (req, res) => {
+  res.render("new");
+});
 
 // INDEX
-breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread
-      }
-    )
-  // res.send(Bread)
-})
- 
+//breads.get('/', (req, res) => {
+//res.render('Index',
+//{
+//breads: Bread
+//}
+//)
+// res.send(Bread)
+//})
+
+breads.get("/", async (req, res) => {
+  const allBreads = await Bread.find();
+
+  res.render("index", {
+    breads: allBreads,
+    title: "index Page",
+  });
+});
+
 // CREATE
-breads.post('/', (req, res) => {
-  console.log(req.body)
+//breads.post('/', (req, res) => {
+//console.log(req.body)
+//if (!req.body.image) {
+//req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+//}
+//if(req.body.hasGluten === 'on') {
+//req.body.hasGluten = true
+//} else {
+//req.body.hasGluten = false
+//}
+//Bread.push(req.body)
+//res.redirect('/breads')
+//})
+
+breads.post("/", (req, res) => {
   if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    req.body.image = undefined;
   }
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
   } else {
-    req.body.hasGluten = false
+    req.body.hasGluten = false;
   }
-  Bread.push(req.body)
-  res.redirect('/breads')
-})
+  Bread.create(req.body);
+  res.redirect("/breads");
+});
+
+breads.get("/:id", (req, res) => {
+  Bread.findById(req.params.id)
+    .then((foundBread) => {
+      res.render("show", {
+        bread: foundBread,
+      });
+    })
+    .catch((err) => {
+      res.send("404");
+    });
+});
+
+//Everything below is not being used
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
+breads.get("/:indexArray/edit", (req, res) => {
+  res.render("edit", {
     bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
-}) 
+    index: req.params.indexArray,
+  });
+});
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-  if(req.body.hasGluten === 'on'){
-    req.body.hasGluten = true
+breads.put("/:arrayIndex", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
   } else {
-    req.body.hasGluten = false
+    req.body.hasGluten = false;
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect('/breads')
-})
+  Bread[req.params.arrayIndex] = req.body;
+  res.redirect("/breads");
+});
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
+breads.get("/:arrayIndex", (req, res) => {
   if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
       index: req.params.arrayIndex,
-    })
+    });
   } else {
-    res.render('404')
+    res.render("404");
   }
-})
+});
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
-})
+breads.delete("/:indexArray", (req, res) => {
+  Bread.splice(req.params.indexArray, 1);
+  res.status(303).redirect("/breads");
+});
 
-module.exports = breads
+module.exports = breads;
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,33 +1,26 @@
-const React = require('react')
-const Default = require('./layouts/Default')
+const React = require("react");
+const Default = require("./layouts/Default");
 
-function Index ({breads})  {
-    return (
-        <Default>
-        <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        {/* This is a JSX comment. */}
-        <div className="newButton">
-          <a href="/breads/new"><button>Add a new bread</button></a>
-        </div>
-        <ul>
-          {
-            breads.map((bread, index)=> {
-              return (<li key={index}>
-                <a href={`/breads/${index}`}>
-                {bread.name}
-                </a>
-              </li>)
-            }) 
-          }
-        </ul>
-      </Default>
-      
-    )
+function Index({ breads }) {
+  return (
+    <Default>
+      <h2>Index Page</h2>
+      <div className="createBreadBtn">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
+      </div>
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${bread.id}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </Default>
+  );
 }
 
-
-module.exports = Index
-
-
-
+module.exports = Index;
